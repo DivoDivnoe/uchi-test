0c9524011b4718072a68665331784a06b68f0f91
Andrey Ivanov <andrey-tm203@mail.ru>
2018-04-22 18:29:31 +0300

    some correctings

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,36 @@
 import Model from './model/Model';
 import Lesson from './lesson/Lesson';
 
-const initLesson = (model) => {
-  const sreenWidth = document.body.clientWidth;
-  const axisLength = sreenWidth > 900 ? 815 : (sreenWidth < 550 ? 300 : 480);
+const AxisLength = {
+  LARGE_SCREEN_AXIS_LENGTH: 815,
+  MEDIUM_SCREEN_AXIS_LENGTH: 480,
+  SMALL_SCREEN_AXIS_LENGTH: 300
+};
 
-  const lesson = new Lesson(model, axisLength);
-  
-  lesson.start();
-}
+const initLesson = model => {
+  const screenWidth = document.body.clientWidth;
+  const axisLength =
+    screenWidth > 900
+      ? AxisLength.LARGE_SCREEN_AXIS_LENGTH
+      : screenWidth < 550
+        ? AxisLength.SMALL_SCREEN_AXIS_LENGTH
+        : AxisLength.MEDIUM_SCREEN_AXIS_LENGTH;
+
+  new Lesson(model, axisLength).start();
+
+  return screenWidth;
+};
 
 document.addEventListener('DOMContentLoaded', () => {
   const model = new Model();
-  
-  initLesson(model);
 
-  window.addEventListener('resize', () => initLesson(model));
+  let screenWidth = initLesson(model);
+
+  window.addEventListener('resize', evt => {
+    const currentScreenWidth = document.body.clientWidth;
+
+    if (currentScreenWidth === screenWidth) return false;
+
+    screenWidth = initLesson(model);
+  });
 });
